2494e9e6d2b39283aa81057cdcb5faca3e6c86c4
Khanh Tran <khanhsbtqk@gmail.com>
2023-02-08 10:40:39 +0700

    add fastify

--- index.js
+++ index.js
@@ -23,3 +23,19 @@ mongodb.then(() => console.log("Connect db success!"));
 app.listen(port || 3000, () => {
   console.log("Server is up on port " + port);
 });
+
+// ESM
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  reply.type("application/json").code(200);
+  return { hello: "world" };
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) throw err;
+  // Server is now listening on ${address}
+});
